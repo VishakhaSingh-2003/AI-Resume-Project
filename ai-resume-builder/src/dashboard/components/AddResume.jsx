04c5562ae3fb3a30f8013d9ad4c2c20987d7ad65
Vishakha Singh <vishakhasingh692003@gmail.com>
2025-08-14 04:53:23 +0530

    add new features

---
+++ ai-resume-builder/src/dashboard/components/AddResume.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { PlusSquare } from "lucide-react";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { v4 as uuidv4 } from "uuid";
+import { useUser } from "@clerk/clerk-react";
+import { Loader2 } from "lucide-react";
+
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import GlobalApi from "./../../../service/GlobalApi";
+
+function AddResume() {
+  const [openDialog, setOpenDialog] = useState(false);
+  const [resumeTitle, setResumeTitle] = useState();
+  const { user } = useUser();
+  const [loading, setLoading] = useState(false);
+
+  const onCreate = () => {
+    setLoading(true);
+    const uuid = uuidv4();
+    const data = {
+      data: {
+        title: resumeTitle,
+        resumeId: uuid,
+        userEmail: user?.primaryEmailAddress?.emailAddress,
+        userName: user?.fullName,
+      },
+    };
+
+    GlobalApi.CreateNewResume(data).then(
+      (resp) => {
+        console.log(resp);
+        if (resp) {
+          setLoading(false);
+        }
+      },
+      (error) => {
+        setLoading(false);
+      }
+    );
+  };
+  return (
+    <div>
+      <div
+        className="w-50 p-14 py-24 border
+         items-center flex 
+         justify-center bg-secondary 
+         rounded-lg h-57 hover:scale-105 transition-all hover:shadow-md cursor-pointer border-dashed"
+        onClick={() => setOpenDialog(true)}>
+        <PlusSquare />
+      </div>
+      <Dialog open={openDialog}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Create New Resume</DialogTitle>
+            <DialogDescription>
+              <span>Add title for your new resume</span>
+              <Input
+                className="my-2"
+                placeholder="Ex.Full Stack Developer"
+                onChange={(e) => setResumeTitle(e.target.value)}
+              />
+            </DialogDescription>
+
+            <div className="flex justify-end gap-5">
+              <Button onClick={() => setOpenDialog(false)} variant="ghost">
+                Cancel
+              </Button>
+              <Button
+                disabled={!resumeTitle || loading}
+                onClick={() => onCreate()}
+                className="bg-[#9f5bff] hover:bg-[#9f5bff] text-white px-4 py-2">
+                {loading ? <Loader2 className="animate-spin" /> : "Create"}
+              </Button>
+            </div>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
+
+export default AddResume;
--- ai-resume-builder/src/dashboard/index.jsx
+++ ai-resume-builder/src/dashboard/index.jsx
@@ -1,9 +1,44 @@
+
+// import React from 'react'
+// import AddResume from './components/AddResume'
+
+// function Dashboard() {
+//   return (
+//     <div className="pt-28 pl-0 pr-10 text-left">
+//       <h2 className="font-bold text-3xl">My Resume</h2>
+//       <p>Start Creating AI resume for your next Job role</p>
+
+//       <div className="mt-10 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-5 justify-start">
+//         <AddResume />
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Dashboard
+
+
 import React from 'react'
+import AddResume from './components/AddResume';
 
 function Dashboard() {
   return (
-    <div className='pt-20'>Dashboard</div>
+    <div className='text-left p-10 md:px-20 lg:px-32'>
+      <h2 className='pt-20 font-bold text-3xl'>My Resume</h2>
+      <p>Start Creating AI resume for your next Job role</p>
+
+      {/* Added mt-10 to create vertical gap */}
+      <div className='mt-10 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-5'>
+        <AddResume />
+      </div>
+    </div>
   )
 }
 
 export default Dashboard;
+
+
+
+
+
+
